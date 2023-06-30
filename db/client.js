19deca01e19d58984e143dfdb553617980fd75dd
Maaya <yamauchimaaya@gmail.com>
2023-06-29 20:40:19 -0500

    updated client

--- db/client.js
+++ db/client.js
@@ -1,15 +1,21 @@
 const { Client } = require("pg");
 
+const databaseUrl = process.env.DATABASE_URL;
+
+if (databaseUrl === undefined) {
+  throw new error("DATABASE_URL environment variable not set");
+}
+
 const client = new Client({
-  connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/capstone",
+  connectionString: databaseUrl,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : undefined,
-
-
 });
+
+console.log("DB_URL: " + process.env.DATABASE_URL);
+console.log("NODE_ENV: "+ process.env.NODE_ENV);
 
 module.exports = {
   client,
